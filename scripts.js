ab3c68b3ebb484217f22bb2443127ef7521f3cf4
Manassanan Rojphientham <manansanun@kirari-2.local>
2026-02-20 00:12:51 +0700

    fix viewport and modal error

--- scripts.js
+++ scripts.js
@@ -3,6 +3,11 @@ const modal = document.getElementById("imgModal");
 const modalImg = document.getElementById("imgPreview");
 const closeBtn = document.querySelector(".img-modal .close");
 
+if (closeBtn) {
+    closeBtn.onclick = () => {
+        modal.style.display = "none";
+    };
+}
 document.querySelectorAll(".cert-img").forEach(img => {
     img.addEventListener("click", () => {
         modal.style.display = "block";
